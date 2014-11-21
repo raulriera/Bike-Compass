8e59fec44621d64b9817714697bb845a10f330b1
Raul Riera <rieraraul@gmail.com>
2014-11-21 20:26:02 +0000

    Moved the network resources into a CityBikesKit framework

---
+++ CityBikesKit/CityBikesKit.h
@@ -0,0 +1,20 @@
+//
+//  CityBikesKit.h
+//  CityBikesKit
+//
+//  Created by Raúl Riera on 21/11/2014.
+//  Copyright (c) 2014 Raúl Riera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CityBikesKit.
+FOUNDATION_EXPORT double CityBikesKitVersionNumber;
+
+//! Project version string for CityBikesKit.
+FOUNDATION_EXPORT const unsigned char CityBikesKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CityBikesKit/PublicHeader.h>
+#import <CityBikesKit/NetworksRepository.h>
+#import <CityBikesKit/StationsRepository.h>
+
--- Podfile
+++ Podfile
@@ -1,6 +1,10 @@
-platform :ios, '8.0'
-pod 'AFNetworking', '~> 2.2'
-pod 'Mantle', '~> 1.5'
+platform :ios, '8.1'
+
 pod 'pop', '~> 1.0'
 pod 'SAMSoundEffect', '~> 0.1'
 pod 'FBAnnotationClustering', '~> 0.1'
+
+target :CityBikesKit, :exclusive => true do
+  pod 'AFNetworking', '~> 2.2'
+  pod 'Mantle', '~> 1.5'
+end
